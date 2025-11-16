2e80bc3d729fae8bf0b19c0eabcb21209a44e7e6
Bengineer <blockengineer42@gmail.com>
2025-11-16 16:59:50 +0000

    refactor: add new_empty constructor to Attribute struct

--- crates/types/src/attribute.rs
+++ crates/types/src/attribute.rs
@@ -6,3 +6,12 @@ pub struct Attribute {
     pub id: Felt,
     pub data: Vec<Felt>,
 }
+
+impl Attribute {
+    pub fn new_empty(id: Felt) -> Attribute {
+        Attribute {
+            id,
+            data: Default::default(),
+        }
+    }
+}
