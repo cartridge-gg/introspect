39c1427956df0e4b215705e25cc3da5cb58a8853
Bengineer <blockengineer42@gmail.com>
2025-11-16 13:15:56 +0000

    refactor: simplify TableSchema structure and remove unused ToValue implementation

--- crates/events/src/types.rs
+++ crates/events/src/types.rs
@@ -1,29 +1,13 @@
+use introspect_types::schema::PrimaryTypeDef;
 use introspect_types::{ColumnDef, FeltIterator, ToValue};
 use serde::{Deserialize, Serialize};
 use starknet_types_core::felt::Felt;
 
-use crate::events::UpdateRecordFields;
-
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct TableSchema {
-    pub table_id: Felt,
-    pub table_name: String,
+    pub id: Felt,
+    pub name: String,
     pub attributes: Vec<String>,
+    pub primary: PrimaryTypeDef,
     pub fields: Vec<ColumnDef>,
-}
-
-impl ToValue for TableSchema {
-    type Value = UpdateRecordFields;
-    fn to_value(&self, data: &mut FeltIterator) -> Option<Self::Value> {
-        Some(UpdateRecordFields {
-            table_id: self.table_id,
-            table_name: self.table_name.clone(),
-            attributes: self.attributes.clone(),
-            fields: self
-                .fields
-                .iter()
-                .map(|f| f.to_value(data))
-                .collect::<Option<Vec<_>>>()?,
-        })
-    }
 }
